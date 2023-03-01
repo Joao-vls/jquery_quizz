3acedb2cb562cd6cd7749afdf3c1930b60f9a632
joa-vls <joa.v55j@gmail.com>
2023-03-01 18:34:13 -0300

    set user

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,5 @@
 function setUser() {
-  const id = "usr"+Math.random().toString(36).substr(2, 9);
+  const id = Math.random().toString(36).substr(2, 9);
   const name = $("#nickname").val();
   let score = 0;
 
@@ -9,7 +9,9 @@ function setUser() {
     score: score
   }
   $.post("http://localhost:3000/users", data,function(){
-    window.location.href="../view/quizz.html?id="+data.id;
+    var url="../view/quizz.html?"+encodeURIComponent(JSON.stringify(data));
+
+    window.location.href=url;
   });
 }
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,70 +1,49 @@
+let param = new URLSearchParams(window.location.search);
+let usuario = JSON.parse(decodeURIComponent(param).slice(0,-1));
 
-var queryString = window.location.search;
-// Cria um objeto URLSearchParams com os parâmetros
-var parametros = new URLSearchParams(queryString);
-// Obtém o valor do parâmetro 'id'
-var idu = parametros.get('id');
-console.log(idu);
 let questions;
+$.ajax({
+	method: "GET",
+	url: "http://localhost:3000/answers/",
+	dataType:"json",
+	success: function(data){
+		questions=data.reverse();
+		getQuestion();
+		console.log(questions);
+	},
+});
 
-//Chama nova pergunta e cria Div
-function getQuestion (idq){
-	try{
-		$.ajax({
-			method: "GET",
-			url: "http://localhost:3000/answers/"+idq,
-			dataType:"json",
-			success: function(data){
-				questions=data;
-				//console.log(questions.id);
-			},
-		});
-	}catch(error){
-		console.error(error);
-	}
+
+
+//Cria Div pergunta
+function getQuestion (){
+
 }
-
 //Confere resposta se correta add score
-function postScore (optionsCheck,idq){
-	$.ajax({
-		method: "GET",
-		url: "http://localhost:3000/answers/"+idq,
-		dataType:"json",
-		success: function(data){
-			if (data.correct_alternative==optionsCheck) {
-				$.ajax({
-					method: "GET",
-					url: "http://localhost:3000/users/"+idu,
-					dataType:"json",
-					success: function(data){
-						console.log(data);
-						let dt={
-							score:parseInt(data.score)+1
-						}
-						$.ajax({
-							method: "PATCH",
-							contentType: "application/json",
-							data: JSON.stringify(dt),
-							url: "http://localhost:3000/users/"+idu,
-							success: function(data){
-								console.log(data);
-							}
-						});
-					}
-				});
-			}
-			//chma outra pergunta
-			getQuestion(idq+1);
-		}
-	});
-}
-getQuestion(0);
+
 $("#next_question").click(()=>{
 	var optionsCheck = $('form input[name="alternative"]')?.filter(':checked')?.val();
 	if (optionsCheck?.length) {
 		console.log(optionsCheck);
-		postScore(optionsCheck,questions.id);
-	}else {
-
+		if (questions.length) {
+			if (questions[questions.length-1].correct_alternative==optionsCheck) {
+				questions[questions.length-1].id;
+				usuario.score+=1
+			}
+			questions.pop();
+			console.log(questions);
+		}
 	}
+	if (!questions.length){
+		$.ajax({
+			method: "PATCH",
+			contentType: "application/json",
+			data: JSON.stringify(usuario),
+			url: "http://localhost:3000/users/"+usuario.id,
+			success: function(data){
+				console.log(data);
+			}
+		});
+	}
+
 });

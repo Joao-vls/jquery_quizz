d2ad325e2e9bc7f1398db9d30e1abaccd09e7d15
joa-vls <joa.v55j@gmail.com>
2023-03-01 11:50:26 -0300

    set user

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,19 +1,18 @@
-function getUser(id) {
-  $.get("https://jquery-quizz-production.up.railway.app/users", {id: id}, function(data){
-    console.log(data[parseInt(id)].score)
-  });
-}
-
 function setUser() {
-  const id = 5;
+  const id = "usr"+Math.random().toString(36).substr(2, 9);
   const name = $("#nickname").val();
-  let score = 150;
-  
+  let score = 0;
+
   const data = {
     id: id,
     name: name,
     score: score
   }
-
-  $.post("https://jquery-quizz-production.up.railway.app/users", data);
+  $.post("https://jquery-quizz-production.up.railway.app/users", data,function(){
+    window.location.href="../view/quizz.html?id="+data.id;
+  });
 }
+
+$("#play_quizz").click(()=>{
+  setUser();
+});
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,66 @@
-let questions = [
-	
-	{
-		number: 1,
-		question: "O que é Jquery?",
-		answer: "Uma biblioteca javascript",
-		options: ["Um método nativo do javascript", "Um plugin", "Uma biblioteca javascript", "Uma"]
-	},
-];
+
+var queryString = window.location.search;
+// Cria um objeto URLSearchParams com os parâmetros
+var parametros = new URLSearchParams(queryString);
+// Obtém o valor do parâmetro 'id'
+var idu = parametros.get('id');
+console.log(idu);
+let questions;
+
+//Chama nova pergunta e cria Div
+function getQuestion (idq){
+	$.ajax({
+		method: "GET",
+		url: "https://jquery-quizz-production.up.railway.app/answers?id="+idq,
+		dataType:"json",
+		success: function(data){
+			questions=data[0];
+			//console.log(questions.id);
+		}
+	});
+}
+
+//Confere resposta se correta add score
+function postScore (optionsCheck,idq){
+	$.ajax({
+		method: "GET",
+		url: "https://jquery-quizz-production.up.railway.app/answers?id="+idq,
+		dataType:"json",
+		success: function(data){
+			if (data[0].correct_alternative==optionsCheck) {
+				$.ajax({
+					method: "GET",
+					url: "https://jquery-quizz-production.up.railway.app/users?id="+idu,
+					dataType:"json",
+					success: function(data){
+						console.log(data);
+						let dt={
+							score:data[0].score+1
+						}
+						$.ajax({
+							method: "PUT",
+							contentType: "application/json",
+							data: JSON.stringify(dt),
+							url: "https://jquery-quizz-production.up.railway.app/users?id="+idu,
+							success: function(data){
+								console.log(data);
+							}
+						});
+					}
+				});
+			}
+			//chma outra pergunta
+			getQuestion(idq+1);
+		}
+	});
+}
+getQuestion(0);
+$("#next_question").click(()=>{
+	var optionsCheck = $('form input[name="alternative"]')?.filter(':checked')?.val();
+	if (optionsCheck?.length) {
+		console.log(optionsCheck);
+		postScore(optionsCheck,questions.id);
+	}else {
+
+	}
+});

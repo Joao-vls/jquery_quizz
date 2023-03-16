13e866813b23689833d515be2442d42cc9370aa4
joa-vls <joa.v55j@gmail.com>
2023-03-16 18:59:48 -0300

    score

--- assets/js/score.js
+++ assets/js/score.js
@@ -3,9 +3,28 @@ let id = decodeURIComponent(param).slice(0,-1);
 $.ajax({
   method: "GET",
   contentType: "application/json",
-  url: "http://localhost:3000/users/"+id,
+  url: "http://localhost:3000/users",
   success: function(data){
     console.log(data);
-    $("body").text(data.name +"  "+data.score)
+    getScore(data);
+    //$("body").text(data.name +"  "+data.score)
   }
 });
+function getScore(data){
+  data.sort(function(a, b) {
+    return b.score - a.score;
+  });
+  for (var i = 0; i < data.length; i++) {
+    var cls=(data[i].id==id) ? `class="you"` : "";
+    var tr=$(` <tr `+cls+`>
+      <td>`+data[i].name+`</td>
+      <td>`+data[i].score+`</td>
+      </tr>
+      `);
+      $("tbody").append(tr);
+    }
+
+  }
+  $(".restart").click(()=>{
+    window.location.href="../../index.html";
+  });

6ef18601ec539b96be39b238d083c1a7c9a783f0
joao <jvr2564@gmail.com>
2023-03-20 17:02:47 -0300

    fix: responsive menu bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
+$(document).ready(() => {
+	$("#btn-mobile").click((e) =>  {
+		if(e.type === 'touchstart') {
+			e.preventDefault();
+		}
 
-$("#btn-mobile").click((e) =>  {
-	if(e.type === 'touchstart') {
-		e.preventDefault();
-	}
+		$("#nav").toggleClass('active');
 
-	$("#nav").toggleClass('active');
+		const active = nav.classList.contains('active')
+		e.currentTarget.setAttribute('aria-expanded', active)
 
-	const active = nav.classList.contains('active')
-	e.currentTarget.setAttribute('aria-expanded', active)
-
-	if (active) {
-		e.currentTarget.setAttribute('aria-label', 'fechar menu')
-	} else {
-		e.currentTarget.setAttribute('aria-label', 'abrir menu')
-	}
-
+		if (active) {
+			e.currentTarget.setAttribute('aria-label', 'fechar menu')
+		} else {
+			e.currentTarget.setAttribute('aria-label', 'abrir menu')
+		}
+	})
 })
